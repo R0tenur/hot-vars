a6867628973aa68cf2219c8911538c73065f773b
Jens Hunt <himself@jenshunt.se>
2025-02-23 16:14:08 +0100

    feat: add basic base type

---
+++ src/HotVars/Hot.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace HotVars;
+
+public class Hot<T> : INotifyPropertyChanged
+{
+    private T _value;
+    public T Value
+    {
+        get => _value;
+        private set
+        {
+            _value = value;
+            OnPropertyChanged(nameof(Value));
+        }
+    }
+
+    internal Hot(T value) => _value = value;
+
+    public void SetValue(T value) => Value = value;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged() =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+
+    public void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    public static bool operator ==(Hot<T> hot, T value) => hot?.Value?.Equals(value) ?? false;
+
+    public static bool operator !=(Hot<T> hot, T value) => !hot?.Value?.Equals(value) ?? false;
+
+    public override bool Equals(object? obj) => _value?.Equals(obj) ?? false;
+
+    public override int GetHashCode() => _value!.GetHashCode();
+
+    public override string ToString() => _value!.ToString() ?? string.Empty;
+}
