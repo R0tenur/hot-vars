bc0338a79e6b0bc0a64da96b2eaf8249d313864c
Jens Hunt <himself@jenshunt.se>
2025-02-23 16:34:44 +0100

    docs: add basic number docs

--- sample/HotVars.Sample/Program.cs
+++ sample/HotVars.Sample/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HotVars;
+
+Console.WriteLine("Example 1: HotNumber");
+HotNumber<int> hotNumber = 42;
+Console.WriteLine(hotNumber); // 42
+hotNumber.PropertyChanged += (sender, args) => Console.WriteLine("HotNumber changed: " + hotNumber);
+hotNumber.SetValue(43); // HotNumber changed: 43
+
+Console.WriteLine();
